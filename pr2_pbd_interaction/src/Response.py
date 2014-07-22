ac4d67a58aab7677bc0f3838df40b1dcf49e2ee6
Maxwell Forbes <m.b.forbes@gmail.com>
2014-07-21 17:05:22 -0700

    Clean up Response.py. Untested.

--- pr2_pbd_interaction/src/Response.py
+++ pr2_pbd_interaction/src/Response.py
@@ -1,109 +1,197 @@
-''' Robot responses in the dialog '''
-import roslib
-roslib.load_manifest('pr2_pbd_interaction')
+'''Robot responses in the dialog.'''
 
+# ######################################################################
+# Imports
+# ######################################################################
+
+# Core ROS imports come first.
+PKG = 'pr2_pbd_interaction'
+import roslib
+roslib.load_manifest(PKG)
+import rospy
+
+# System builtins
+import os
+
+# ROS builtins
 from actionlib import SimpleActionClient
-from RobotSpeech import RobotSpeech
-from pr2_social_gaze.msg import GazeGoal, GazeAction
+
+# ROS 3rd party
 from sound_play.msg import SoundRequest
 from sound_play.libsoundplay import SoundClient
-import os
+
+# Local
+from RobotSpeech import RobotSpeech
+from pr2_social_gaze.msg import GazeGoal, GazeAction
 from pr2_pbd_interaction.msg import RobotSound
 
 
+# ######################################################################
+# Module level constants
+# ######################################################################
+
+# Note the constant PKG above should be set.
+
+# Directory sounds are in, within the package.
+SOUNDS_DIR = os.path.join(roslib.packages.get_pkg_dir(PKG), 'sounds', '')
+
+# Common postfix for sound files.
+SOUND_FILEFORMAT = '.wav'
+
+# Name of sound file for unrecognized sounds.
+SOUND_UNKNOWN = 'OTHER'
+
+# The ROS action for head gazes.
+ACTION_GAZE = 'gaze_action'
+
+
+# ######################################################################
+# Classes
+# ######################################################################
+
 class Response:
-    "Unit of interaction, explains how to respond to a speech command"
-    # Static stuff
+    '''Unit of interaction, explains how to respond to a speech
+    command. A new instance is made for each type of response.
+    '''
+
+    # Variables that will be set later.
     gaze_client = None
     _sound_client = None
     _robot_speech = None
-    _sounds_dir = roslib.packages.get_pkg_dir('pr2_pbd_interaction')
-    _sounds_dir = os.path.join(_sounds_dir, 'sounds', '')
+
+    # Gaze goals.
     glance_actions = [GazeGoal.GLANCE_RIGHT_EE, GazeGoal.GLANCE_LEFT_EE]
     follow_actions = [GazeGoal.FOLLOW_RIGHT_EE, GazeGoal.FOLLOW_LEFT_EE]
-    open_responses = [RobotSpeech.RIGHT_HAND_OPENING,
-                      RobotSpeech.LEFT_HAND_OPENING]
-    close_responses = [RobotSpeech.RIGHT_HAND_CLOSING,
-                       RobotSpeech.LEFT_HAND_CLOSING]
-    release_responses = [RobotSpeech.RIGHT_ARM_RELEASED,
-                         RobotSpeech.LEFT_ARM_RELEASED]
-    hold_responses = [RobotSpeech.RIGHT_ARM_HOLDING,
-                      RobotSpeech.LEFT_ARM_HOLDING]
-    already_open_responses = [RobotSpeech.RIGHT_HAND_ALREADY_OPEN,
-                            RobotSpeech.LEFT_HAND_ALREADY_OPEN]
-    already_closed_responses = [RobotSpeech.RIGHT_HAND_ALREADY_CLOSED,
-                              RobotSpeech.LEFT_HAND_ALREADY_CLOSED]
-    already_holding_responses = [RobotSpeech.RIGHT_ARM_ALREADY_HOLDING,
-                               RobotSpeech.LEFT_ARM_ALREADY_HOLDING]
-    already_released_responses = [RobotSpeech.RIGHT_ARM_ALREADY_RELEASED,
-                                RobotSpeech.LEFT_ARM_ALREADY_RELEASED]
 
-    all_sounds = [RobotSound.ALL_POSES_DELETED, RobotSound.ERROR,
-                 RobotSound.MICROPHONE_WORKING, RobotSound.POSE_SAVED,
-                 RobotSound.START_TRAJECTORY, RobotSound.CREATED_ACTION,
-                 RobotSound.EXECUTION_ENDED, RobotSound.OTHER,
-                 RobotSound.STARTING_EXECUTION, RobotSound.SUCCESS]
+    # Responses (robot speech).
+    # Open
+    open_responses = [
+        RobotSpeech.RIGHT_HAND_OPENING, RobotSpeech.LEFT_HAND_OPENING]
+    already_open_responses = [
+        RobotSpeech.RIGHT_HAND_ALREADY_OPEN,
+        RobotSpeech.LEFT_HAND_ALREADY_OPEN]
+
+    # Close
+    close_responses = [
+        RobotSpeech.RIGHT_HAND_CLOSING, RobotSpeech.LEFT_HAND_CLOSING]
+    already_closed_responses = [
+        RobotSpeech.RIGHT_HAND_ALREADY_CLOSED,
+        RobotSpeech.LEFT_HAND_ALREADY_CLOSED]
+
+    # Release (relax).
+    release_responses = [
+        RobotSpeech.RIGHT_ARM_RELEASED, RobotSpeech.LEFT_ARM_RELEASED]
+    already_released_responses = [
+        RobotSpeech.RIGHT_ARM_ALREADY_RELEASED,
+        RobotSpeech.LEFT_ARM_ALREADY_RELEASED]
+
+    # Hold (freeze).
+    hold_responses = [
+        RobotSpeech.RIGHT_ARM_HOLDING, RobotSpeech.LEFT_ARM_HOLDING]
+    already_holding_responses = [
+        RobotSpeech.RIGHT_ARM_ALREADY_HOLDING,
+        RobotSpeech.LEFT_ARM_ALREADY_HOLDING]
+
+    # Sounds (robot sound).
+    all_sounds = [
+        RobotSound.ALL_POSES_DELETED,
+        RobotSound.ERROR,
+        RobotSound.MICROPHONE_WORKING,
+        RobotSound.POSE_SAVED,
+        RobotSound.START_TRAJECTORY,
+        RobotSound.CREATED_ACTION,
+        RobotSound.EXECUTION_ENDED,
+        RobotSound.OTHER,
+        RobotSound.STARTING_EXECUTION,
+        RobotSound.SUCCESS
+    ]
 
     def __init__(self, function_to_call, function_param):
+        '''
+        Args:
+            function_to_call (function): Upon respond(...), the function
+                that should be called.
+            function_param (Object): Upon respond(...), when calling
+                function_to_call, the argument that should be passed in.
+                Note that unlike almost every other instance of Object
+                in the code, this could be any python Object, not the
+                Object.msg Object.
+        '''
         self.function_to_call = function_to_call
         self.function_param = function_param
 
-        if (Response.gaze_client == None):
-            Response.gaze_client = SimpleActionClient('gaze_action',
-                                                       GazeAction)
+        if Response.gaze_client is None:
+            Response.gaze_client = SimpleActionClient(ACTION_GAZE, GazeAction)
             Response.gaze_client.wait_for_server()
 
-        if (Response._robot_speech == None):
+        if Response._robot_speech is None:
             Response._robot_speech = RobotSpeech()
 
-        if (Response._sound_client == None):
+        if Response._sound_client is None:
             Response._sound_client = SoundClient()
 
     def respond(self):
-        ''' Triggers the defined response'''
+        '''Triggers the response (that has already been set).'''
         speech_resp, gaze_resp = self.function_to_call(self.function_param)
         # Speech response
-        if (speech_resp != None):
+        if speech_resp is not None:
             Response.say(speech_resp)
             Response.respond_with_sound(speech_resp)
         # Gaze response
-        if (gaze_resp != None):
+        if gaze_resp is not None:
             Response.perform_gaze_action(gaze_resp)
 
     @staticmethod
     def perform_gaze_action(gaze_action):
-        ''' Triggers a gaze action'''
+        '''Triggers a gaze action.
+
+        Args:
+            gaze_action (int): One of the constants defined in
+                Gaze.action.
+        '''
         goal = GazeGoal()
         goal.action = gaze_action
         Response.gaze_client.send_goal(goal)
 
     @staticmethod
     def look_at_point(point):
-        ''' Looks at a specific point'''
-        Response.gaze_client.send_goal(
-                        GazeGoal(GazeGoal.LOOK_AT_POINT, point))
+        '''Looks at a specific point.
+
+        Args:
+            point (Point)
+        '''
+        Response.gaze_client.send_goal(GazeGoal(GazeGoal.LOOK_AT_POINT, point))
 
     @staticmethod
     def say(speech_resp):
-        ''' Triggers a speech action'''
+        '''Triggers a speech action.
+
+        Args:
+            speech_resp (str): What to say.
+        '''
         Response._robot_speech.say(speech_resp)
 
     @staticmethod
     def respond_with_sound(speech_resp):
-        ''' Triggers a sound response'''
-        if (speech_resp == RobotSpeech.STEP_RECORDED):
+        '''Triggers a sound response.
+
+        Args:
+            speech_resp (str): The speech (concept) that we want to
+                convey. Should be one of RobotSpeech.*
+        '''
+        if speech_resp == RobotSpeech.STEP_RECORDED:
             Response.play_sound(RobotSound.POSE_SAVED)
-        elif (speech_resp == RobotSpeech.TEST_RESPONSE):
+        elif speech_resp == RobotSpeech.TEST_RESPONSE:
             Response.play_sound(RobotSound.MICROPHONE_WORKING)
-        elif (speech_resp == RobotSpeech.SKILL_CLEARED):
+        elif speech_resp == RobotSpeech.SKILL_CLEARED:
             Response.play_sound(RobotSound.ALL_POSES_DELETED)
-        elif (RobotSpeech.START_EXECUTION in speech_resp):
+        elif RobotSpeech.START_EXECUTION in speech_resp:
             Response.play_sound(RobotSound.STARTING_EXECUTION)
-        elif (speech_resp == RobotSpeech.EXECUTION_ENDED):
+        elif speech_resp == RobotSpeech.EXECUTION_ENDED:
             Response.play_sound(RobotSound.EXECUTION_ENDED)
-        elif (speech_resp == RobotSpeech.STARTED_RECORDING_MOTION):
+        elif speech_resp == RobotSpeech.STARTED_RECORDING_MOTION:
             Response.play_sound(RobotSound.START_TRAJECTORY)
-        elif (RobotSpeech.SKILL_CREATED in speech_resp):
+        elif RobotSpeech.SKILL_CREATED in speech_resp:
             Response.play_sound(RobotSound.CREATED_ACTION)
         elif (speech_resp == RobotSpeech.START_STATE_RECORDED or
               speech_resp == RobotSpeech.STOPPED_RECORDING_MOTION or
@@ -132,11 +220,14 @@ class Response:
 
     @staticmethod
     def play_sound(requested_sound):
-        ''' Plays the requested sound'''
-        if (requested_sound in Response.all_sounds):
-            Response._sound_client.playWave(os.path.join(Response._sounds_dir,
-                                              requested_sound + '.wav'))
-        else:
-            Response._sound_client.playWave(os.path.join(Response._sounds_dir,
-                                                         'OTHER.wav'))
+        '''Plays the requested sound.
 
+        Args:
+            requested_sound (str): One of the constants in RobotSound.*.
+        '''
+        if requested_sound in Response.all_sounds:
+            Response._sound_client.playWave(
+                os.path.join(SOUNDS_DIR, requested_sound + SOUND_FILEFORMAT))
+        else:
+            Response._sound_client.playWave(
+                os.path.join(SOUNDS_DIR, SOUND_UNKNOWN + SOUND_FILEFORMAT))
