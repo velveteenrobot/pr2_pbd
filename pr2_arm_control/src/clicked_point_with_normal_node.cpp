fc1a09e54d37a702afdaf9fa06bde816ffd9b35a
Dan Butler <djbutler@cs.washington.edu>
2016-02-24 00:18:53 -0800

    rviz point normal clicking works

--- pr2_arm_control/src/clicked_point_with_normal_node.cpp
+++ pr2_arm_control/src/clicked_point_with_normal_node.cpp
@@ -13,16 +13,9 @@ int main (int argc, char** argv)
 {
   // Initialize ROS
   ros::init (argc, argv, "clicked_point_with_normal_node");
-  ros::NodeHandle nh;
 
   // Create cloud buffer object
   pr2_arm_control::NormalBuffer buffer;
-
-  // subscriber for input point cloud
-  ros::Subscriber cloud_sub = nh.subscribe ("/head_mount_kinect/depth_registered/points", 1, &pr2_arm_control::NormalBuffer::cloud_cb, &buffer);
-  
-  // subscriber for input clicked point
-  ros::Subscriber point_sub = nh.subscribe ("/clicked_point", 1, &pr2_arm_control::NormalBuffer::clicked_point_cb, &buffer);
 
   // Spin
   ros::spin ();
